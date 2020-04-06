62555fb4ec8c862451ee6de161484f350a196b3f
Jacob Lever <jacobianism@gmail.com>
2020-04-06 10:08:01 +0100

    End conferences wen someone leaves and don't assign conference until call is ready to join it

--- index.js
+++ index.js
@@ -43,7 +43,10 @@ let parseQuery = (queryString) => {
 }
 
 let getOrCreateConference = (callId) => {
-  let conferencesWithSpace = db.filter(x => x.participants.length < 2);
+  console.log(`Call ${callId}: db ${db}`);
+  let conferencesWithSpace = db.filter(x => {
+    return x.participants.length < 2 && x.ended === false
+  });
   
   if (conferencesWithSpace.length > 0) {
     let conferenceToJoin = conferencesWithSpace[0];
@@ -55,6 +58,7 @@ let getOrCreateConference = (callId) => {
   let conference = {
     name: conferenceName,
     participants: [callId],
+    ended: false,
   }
   db.push(conference);
   return conference;
@@ -67,23 +71,58 @@ let db = [];
 
 app.post('/', (request, response) => {
   let welcomeMessage = "Welcome to COVID Caller! Hold on a moment while we find someone for you to talk to.";
-  let timeNow = Date.now();
 
   getBody(request).then((body) => {
-    
-    let callerNumber = body['Caller'];
     let callId = body['CallSid'];
-    var conference = getOrCreateConference(callId);
 
+    console.log(`Call ${callId}: Sending welcome message...`)
     let responseBody = `
 <Response>
   <Say voice="alice">${welcomeMessage}</Say>
-  <Dial>
-    <Conference maxParticipants="2">${conference.name}</Conference>
-  </Dial>
+  <Redirect method="POST">./find-friend</Redirect>
 </Response>`;
   respondWithXml(response, responseBody);
   });
-});
+})
+
+app.post('/find-friend', (request, response) => {
+  getBody(request).then((body) => {
+    let callId = body['CallSid'];
+
+    console.log(`Call ${callId}: Finding Conference...`);
+    var conference = getOrCreateConference(callId);
+    console.log(`Call ${callId}: Joining conference ${conference.name}`);
+
+    let endMessage = "Your partner has hung up. We hope you enjoyed your conversation with them. See you next time, Goodbye!";
+    let responseBody = `
+<Response>
+  <Dial>
+    <Conference
+      maxParticipants="2"
+      statusCallback="./conference-callback"
+      statusCallbackEvent="leave"
+      endConferenceOnExit="true">
+        ${conference.name}
+    </Conference>
+  </Dial>
+  <Say voice="alice">${endMessage}</Say>
+</Response>`;
+  respondWithXml(response, responseBody);
+  });
+})
+
+app.post('/conference-callback', (request, response) => {
+  getBody(request).then((body) => {
+    let callId = body['CallSid'];
+    if (body['StatusCallbackEvent'] === 'participant-leave') {
+      let conferenceName = body['FriendlyName'];
+      let conference = db.filter(x => x.name === conferenceName)[0];
+      if (conference) {
+        console.log(`Call ${callId}: Left conference ${conference.name}`);
+        conference.ended = true;
+      }
+    }
+  });
+})
 
 app.listen(port, () => console.log(`COVID Caller listening at http://localhost:${port}`))
